91cba53787f805498816a102ac907e9d488b06f7
blek! <creeperywime@gmail.com>
2022-07-04 01:05:32 +1000

    now it can \q, \q() and \h

--- interpreter.h
+++ interpreter.h
@@ -38,6 +38,62 @@ int int_proc_char() {
 	
 }
 
+int int_print_help() {
+	printf(
+// help
+"\n"
+"---\n"
+"BrainHeck commands\n"
+"\\h   - Display help about functions (should be used in the interpreter only)\n"
+"\\d   - Print the current pointer value as decimal number\n"
+"\\n   - Print the current pointer position\n"
+"\\q   - Exit the program\n"
+"\\\\   - No-op, if you want to use \ symbol in comments about code so desperately\n"
+"\\s() - Run some system command, like \s(clear) will clear the screen\n"
+"\\p() - Print message, like \p(Hello world) is a basic hello world in brainHeck\n"
+"\\q() - Exit program with exit code, like \q(60) will exit the program with exit code 60. Non-number value like \q(abc) should produce a critical error.\n"
+"\\j() - Jump to pointer value, like \j(2) will jump to pointer at index 2 (3rd position in array)\n"
+"\\f() - Write the pointer value to file in append mode.\n"
+"\\a() - Delete file\n"
+"\\s() - Jump to char at index\n"
+"\\i() - Include contents of another file\n"
+"---\n"
+);
+}
+
+int int_proc_cmd(char* pr, size_t index) {
+	index++;
+	switch(pr[index]) {
+		case 'd':
+			printf("%d", int_program[int_ptr]);
+			break;
+		case 'q':
+			if (pr[index+1] == '(') {
+				int exitCode = 0;
+				int offset = 1;
+				if (pr[index+2] == '-') offset=2;				
+				
+				for (size_t i = index+offset; pr[i] != ')'; i++) {
+					if (pr[i] >= 48 && pr[i] <= 57) {
+						exitCode = (exitCode * 10) + (pr[i] - 48);
+					}
+					if (i == strlen(pr)) {
+						printf("\nError: unmatched \"(\" at index %d\n", index);
+						exit(-1);
+					}
+				}
+				if (offset == 2) exitCode *= -1;
+				exit(exitCode);
+			}
+			printf("Bye\n");
+			exit(0);
+			break;
+		case 'h':
+			int_print_help();
+			break;
+	}
+}
+
 #define array_resize(arr, unitsize, newsz) arr = realloc(arr, (newsz) * (unitsz)); arr[newsz] = 0;
 
 int int_run() {
@@ -55,7 +111,6 @@ int int_run() {
 		
 		for (size_t i = 0; i != strlen(in); i++) {
 			switch (in[i]) {
-				case 'q': exit(0);
 				case '+':
 					if (int_program[int_ptr] == 0xFF) {
 						bh_err(i, "<stdin>", "Attempt to increase pointer to more than max char value (255).");
@@ -97,9 +152,8 @@ int int_run() {
 						i = loops[scope];
 					}
 					break;
-				case 'd':
-					printf("%d", int_program[int_ptr]);
-					break;
+				case '\\':
+					int_proc_cmd(in, i);
 			}
 		}
 	}
