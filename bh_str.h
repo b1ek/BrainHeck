59e8ce5a310ef910418a488aa7c45a0f897b08f2
b1ek <creeperywime@gmail.com>
2022-07-08 23:53:16 +1000

    strrev error fix

--- bh_str.h
+++ bh_str.h
@@ -4,6 +4,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef strrev
+// Some compilers dont support strrev()
+char* strrev(char *str) {
+	char *p1;
+	char *p2;
+
+	if (! str || ! *str) return str;
+	for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2) {
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+	return str;
+}
+#endif //strrev
+
 size_t bh_strlen(char* s) {
 	int i = 0;
 	for (; s[i] == '\0'; i++) {}
